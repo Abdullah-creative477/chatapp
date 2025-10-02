877b4c8fe26715fd9dc3310600f5ca297a0a385f
Abdullah-creative477 <abham3489@gmail.com>
2025-10-03 00:40:18 +0500

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 const API_URL = import.meta.env.PROD 
-  ? "https://chatapp-backend-abdullah.onrender.com"  // We'll update this later
+  ? "https://chatapp-production-a6c7.up.railway.app/"  // We'll update this later
   : "http://localhost:5000";
 
 export default API_URL;
